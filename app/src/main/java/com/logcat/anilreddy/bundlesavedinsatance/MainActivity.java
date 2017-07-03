0406581ff4cb62e1251636ce00b4df648b2a28b2
AnilRdyAleti <anilreddy.1323@gmail.com>
2017-07-03 14:20:49 -0400

    bundle saved instance

--- app/src/main/java/com/logcat/anilreddy/bundlesavedinsatance/MainActivity.java
+++ app/src/main/java/com/logcat/anilreddy/bundlesavedinsatance/MainActivity.java
@@ -1,13 +1,33 @@
 package com.logcat.anilreddy.bundlesavedinsatance;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    int counter =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        counter++;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("counter",counter);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        counter = savedInstanceState.getInt("counter");
     }
 }
